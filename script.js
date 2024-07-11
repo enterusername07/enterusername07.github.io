9d3d37c3bc6f0fbb286c0a335983a1b1f5f0ec10
enterusername07 <rosalindagallego7@gmail.com>
2024-07-11 10:40:46 +0800

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ayieeeeee, labyuuuu ohahahahah.";
+  question.innerHTML = "Ayieeeeee, labyuuuu too ohahahahah.";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
