a3202966e29118ff615e77d726fc9b1839335e92
enterusername07 <rosalindagallego7@gmail.com>
2024-07-11 10:38:52 +0800

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ayieeeeee, labyuuuu hahahahah.";
+  question.innerHTML = "Ayieeeeee, labyuuuu ohahahahah.";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
